c587d4b2b44a0f86909604ed8f52c7f90f89e299
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2024-08-26 16:27:30 -0400

    Modal

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,51 @@
-import './Navbar.css'
-import img1 from '../../assets/image/menu.png'
-import img2 from '../../assets/image/logo.png'
+// import './Navbar.css'
+// import img1 from '../../assets/image/menu.png'
+// import img2 from '../../assets/image/logo.png'
 
 
-function Navbar(){
-    return(
+// function Navbar(){
+//     return(
+//         <div className='navbar'>
+//         <img className='menu' src={img1}></img>
+//         <img className='logo' src={img2}></img>
+//     </div>
+//     )
+// }
+
+// export default Navbar
+
+import React, { useState } from 'react';
+import './Navbar.css';
+import img1 from '../../assets/image/menu.png';
+import img2 from '../../assets/image/logo.png';
+
+function Navbar() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    return (
         <div className='navbar'>
-        <img className='menu' src={img1}></img>
-        <img className='logo' src={img2}></img>
-    </div>
-    )
+            <img className='menu' src={img1} onClick={toggleModal} alt="menu"></img>
+            <img className='logo' src={img2} alt="logo"></img>
+
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                    <img className='menu' src={img1} onClick={toggleModal} alt="menu"></img>
+                        <h1 className='tittle-modal'>Listas de atajos</h1>
+                        <ul>
+                            <li onClick={toggleModal}>Básicos</li>
+                            <li onClick={toggleModal}>Intermedio</li>
+                            <li onClick={toggleModal}>Avanzado</li>
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
