798b3a253fcc9ed3fa88b150a80c493b3c6baddc
Giova2002 <cianfaglione.giovanna@correo.unimet.edu.ve>
2024-09-24 16:17:43 -0400

    modificaciones

--- Backend/firebase_config.py
+++ Backend/firebase_config.py
@@ -1,9 +1,27 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
-cred = credentials.Certificate("Backend/chatbot-e10ff-firebase-adminsdk-o5erg-1fda4a84aa.json")
-firebase_admin.initialize_app(cred)
+# Cargar las credenciales desde la variable de entorno
+firebase_credentials = os.environ.get("FIREBASE_CREDENTIALS")
+
+if firebase_credentials:
+    cred_dict = json.loads(firebase_credentials)
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
+else:
+    raise ValueError("Firebase credentials not found in environment variables")
 
 db = firestore.client()
+
+
+# import firebase_admin
+# from firebase_admin import credentials, firestore
+
+# cred = credentials.Certificate("Backend/chatbot-e10ff-firebase-adminsdk-o5erg-1fda4a84aa.json")
+# firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
 
 # Backend/chatbot-e10ff-firebase-adminsdk-o5erg-8e7b8736bf.json
